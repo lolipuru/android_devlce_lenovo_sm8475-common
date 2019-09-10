1cf44adb22c9b6d07441db0ae841b41b0f2c7332
LuK1337 <priv.luk@gmail.com>
2019-09-10 19:02:00 +0200

    sm8475-common: Copy up to date handheld_core_hardware
    Change-Id: I458867b3b322ad754b78877209b7f4d8fb78eb38

--- common.mk
+++ common.mk
@@ -242,6 +242,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \

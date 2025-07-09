a63dc909030df1a179ed81ae561609e1321dee88
lolipuru <banstellazz@gmail.com>
2025-07-10 00:42:14 +0700

    sm8475-common: full override usb script from stock zui 15
    peripheral mode didn't works with clo usb scripts due to lenovo implement their own usb node
    
    todo: commonized with asphalt and merged latest usb from clo
    
    Change-Id: I384fceaf4d373782da864068f345f362a6c673f4
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- common.mk
+++ common.mk
@@ -268,8 +268,8 @@ PRODUCT_PACKAGES += \
     android.hardware.usb.gadget-service.qti
 
 PRODUCT_PACKAGES += \
-    init.qcom.usb.rc \
-    init.qcom.usb.sh \
+    init.qcom.usb.rc.lenovo \
+    init.qcom.usb.sh.lenovo \
     lenovo_usb_compositions.conf
 
 PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
